edf7f54375ad0212bf0ac905f1da1aa6159fda26
Jonatandb <jonatandb@gmail.com>
2020-10-12 13:51:47 -0300

    1.8: unicafe step3
    https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+    const all = good + neutral + bad
+    const average = all / 3
+    const positivePercent = (good * 100 / all) || 0
+    return <>
+        <h1>statistics</h1>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {all}</div>
+        <div>average {average}</div>
+        <div>positive {positivePercent} %</div>
+    </>
+}
+
 const App = () => {
     // save clicks of each button to own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-
-    const all = good + neutral + bad
-    const average = all / 3
-    const positivePercent = (good * 100 / all) || 0
 
     return (
         <>
@@ -16,16 +27,9 @@ const App = () => {
             <button onClick={() => setGood(good + 1)}>good</button>
             <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
             <button onClick={() => setBad(bad + 1)}>bad</button>
-            <h1>statistics</h1>
-            <div>good {good}</div>
-            <div>neutral {neutral}</div>
-            <div>bad {bad}</div>
-            <div>all {all}</div>
-            <div>average {average}</div>
-            <div>positive {positivePercent} %</div>
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </>
     )
 }
-
 
 export default App
