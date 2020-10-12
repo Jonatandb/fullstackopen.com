4971fbaf393c28b4ad5500ac1e93edc589063f47
Jonatandb <jonatandb@gmail.com>
2020-10-12 14:48:05 -0300

    1.12*: anecdotes step1
    https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,25 @@
 import React, { useState } from 'react'
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
-const Statistic = ({ text, value }) => (
-    <tr>
-        <td>{text}</td>
-        <td>{value}</td>
-    </tr>
-)
-
-const Statistics = ({ good, neutral, bad }) => {
-    const all = good + neutral + bad
-    const average = all / 3
-    const positivePercent = (good * 100 / all) || 0
-    const content = <>
-        <Statistic text="good" value={good} />
-        <Statistic text="neutral" value={neutral} />
-        <Statistic text="bad" value={bad} />
-        <Statistic text="all" value={all} />
-        <Statistic text="average" value={average} />
-        <Statistic text="positive" value={`${positivePercent} %`} />
-    </>
-
-    return <table>
-        <tbody>
-            {all > 0 ? content : <tr colSpan={2}><td>No feedback given</td></tr>}
-        </tbody>
-    </table >
-
-}
+const randomQuoteIndexNumber = () => Math.floor(Math.random() * anecdotes.length)
 
 const App = () => {
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+    const [selected, setSelected] = useState(randomQuoteIndexNumber())
 
-    return (
-        <>
-            <h1>give feedback</h1>
-            <Button onClick={() => setGood(good + 1)} text="good" />
-            <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-            <Button onClick={() => setBad(bad + 1)} text="bad" />
-            <h1>statistics</h1>
-            <Statistics good={good} neutral={neutral} bad={bad} />
-        </>
+    return (<>
+        <div>
+            {anecdotes[selected]}
+        </div>
+        <button onClick={() => setSelected(randomQuoteIndexNumber())}>next anecdote</button>
+    </>
     )
 }
 
