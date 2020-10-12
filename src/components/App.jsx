e838ffed9844f7f2107b6de4693bd73a9e34f847
Jonatandb <jonatandb@gmail.com>
2020-10-12 13:31:38 -0300

    1.6: unicafe step1
    https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,24 @@
-import React from 'react'
-import Header from './Header'
-import Content from './Content'
-import Total from './Total'
+import React, { useState } from 'react'
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
     return (
-        <div>
-            <Header course={course} />
-            <Content course={course} />
-            <Total course={course} />
-        </div >
+        <>
+            <h1>give feedback</h1>
+            <button onClick={() => setGood(good + 1)}>good</button>
+            <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+            <button onClick={() => setBad(bad + 1)}>bad</button>
+            <h1>statistics</h1>
+            <div>good {good}</div>
+            <div>neutral {neutral}</div>
+            <div>bad {bad}</div>
+        </>
     )
 }
+
+
 export default App
