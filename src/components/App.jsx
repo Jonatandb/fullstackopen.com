04afd5192ef0498781487abbd671f1da07f918d4
Jonatandb <jonatandb@gmail.com>
2020-10-12 14:36:31 -0300

    1.11*: unicafe step6
    https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,12 @@ import React, { useState } from 'react'
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Statistic = ({ text, value }) => <div>{text} {value}</div>
+const Statistic = ({ text, value }) => (
+    <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+    </tr>
+)
 
 const Statistics = ({ good, neutral, bad }) => {
     const all = good + neutral + bad
@@ -17,14 +22,15 @@ const Statistics = ({ good, neutral, bad }) => {
         <Statistic text="positive" value={`${positivePercent} %`} />
     </>
 
-    return <>
-        <h1>statistics</h1>
-        { all > 0 ? content : <h3>No feedback given</h3>}
-    </>
+    return <table>
+        <tbody>
+            {all > 0 ? content : <tr colSpan={2}><td>No feedback given</td></tr>}
+        </tbody>
+    </table >
+
 }
 
 const App = () => {
-    // save clicks of each button to own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
@@ -35,6 +41,7 @@ const App = () => {
             <Button onClick={() => setGood(good + 1)} text="good" />
             <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
             <Button onClick={() => setBad(bad + 1)} text="bad" />
+            <h1>statistics</h1>
             <Statistics good={good} neutral={neutral} bad={bad} />
         </>
     )
