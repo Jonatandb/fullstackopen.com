7fc707a68b986c3d407cb3eb6a5a3109e06dbddb
Jonatandb <jonatandb@gmail.com>
2020-10-13 20:14:27 -0300

    2.4: Course information step9
    2.5: separate module
    
    https://fullstackopen.com/en/part2/rendering_a_collection_modules

--- part2/src/components/Total.jsx
+++ part2/src/components/Total.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 
-const Total = ({ course: { parts } }) => <h4>Number of exercises {parts.reduce((total, part) => {
-    // console.log('2.3*: Course information step8 - https://fullstackopen.com/en/part2/rendering_a_collection_modules');
-    return total += part.exercises
-}, 0)}</h4>
+const Total = ({ course: { parts } }) => <h3>total of {parts.reduce((total, part) => total += part.exercises, 0)} exercises</h3>
 
 export default Total
