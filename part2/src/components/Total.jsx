76b546a574f9b310c345752afb737cf5351e1b1a
Jonatandb <jonatandb@gmail.com>
2020-10-13 20:04:08 -0300

    2.2: Course information step7
    https://fullstackopen.com/en/part2/rendering_a_collection_modules

--- part2/src/components/Total.jsx
+++ part2/src/components/Total.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const Total = ({ course: { parts } }) => <p>Number of exercises {parts.reduce((total, part) => total += part.exercises, 0)}</p>
+const Total = ({ course: { parts } }) => <h4>Number of exercises {parts.reduce((total, part) => total += part.exercises, 0)}</h4>
 
 export default Total
