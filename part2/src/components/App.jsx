738f7ece7a453ae999346e2627ed6f7ffaf83785
Jonatandb <jonatandb@gmail.com>
2020-10-13 21:06:29 -0300

    2.6: The Phonebook Step1
    https://fullstackopen.com/en/part2/forms

--- part2/src/components/App.jsx
+++ part2/src/components/App.jsx
@@ -1,54 +1,35 @@
 import React from 'react'
-import Course from './Course'
+import { useState } from 'react'
 
 const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Jonatan Db' }
+    ])
 
-    const courses = [
-        {
-            name: 'Half Stack application development',
-            id: 1,
-            parts: [
-                {
-                    name: 'Fundamentals of React',
-                    exercises: 10,
-                    id: 1
-                },
-                {
-                    name: 'Using props to pass data',
-                    exercises: 7,
-                    id: 2
-                },
-                {
-                    name: 'State of a component',
-                    exercises: 14,
-                    id: 3
-                },
-                {
-                    name: 'Redux',
-                    exercises: 11,
-                    id: 4
-                }
-            ]
-        },
-        {
-            name: 'Node.js',
-            id: 2,
-            parts: [
-                {
-                    name: 'Routing',
-                    exercises: 3,
-                    id: 1
-                },
-                {
-                    name: 'Middlewares',
-                    exercises: 7,
-                    id: 2
-                }
-            ]
-        }
-    ]
+    const [newName, setNewName] = useState('')
 
-    return courses.map(course => <Course key={course.id} course={course} />)
+    const handleChange = evt => setNewName(evt.target.value)
+
+    const handleSubmit = evt => {
+        evt.preventDefault()
+        setPersons(persons.concat({ name: newName }))
+        setNewName('')
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    name: <input value={newName} onChange={handleChange} />
+                </div>
+                <div>
+                    <button type='submit'>add</button></div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map(p => <div key={p.name}>{p.name}</div>)}
+        </div>
+    )
 }
 
 export default App
