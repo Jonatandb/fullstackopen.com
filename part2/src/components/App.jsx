4b988f6d9c110125bd61c504853bcb427c9941ca
Jonatandb <jonatandb@gmail.com>
2020-10-13 21:19:38 -0300

    2.8: The Phonebook Step3
    https://fullstackopen.com/en/part2/forms

--- part2/src/components/App.jsx
+++ part2/src/components/App.jsx
@@ -3,20 +3,23 @@ import { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Jonatan Db' }
+        { name: 'Jonatan Db', number: '123-345-567' }
     ])
 
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
-    const handleChange = evt => setNewName(evt.target.value)
+    const handleChangeName = evt => setNewName(evt.target.value)
+    const handleChangeNumber = evt => setNewNumber(evt.target.value)
 
     const handleSubmit = evt => {
         evt.preventDefault()
         if (persons.map(p => p.name).includes(newName)) {
             alert(`${newName} is already added to phonebook`)
         } else {
-            setPersons(persons.concat({ name: newName }))
+            setPersons([...persons, { name: newName, number: newNumber }])
             setNewName('')
+            setNewNumber('')
         }
     }
 
@@ -25,13 +28,16 @@ const App = () => {
             <h2>Phonebook</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                    name: <input value={newName} onChange={handleChange} />
+                    name: <input value={newName} onChange={handleChangeName} />
+                </div>
+                <div>
+                    number: <input value={newNumber} onChange={handleChangeNumber} />
                 </div>
                 <div>
                     <button type='submit'>add</button></div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(p => <div key={p.name}>{p.name}</div>)}
+            {persons.map(p => <div key={p.name}>{p.name} {p.number}</div>)}
         </div>
     )
 }
