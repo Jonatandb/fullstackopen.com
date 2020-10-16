b9e558dd74b3b6b8cf1c4d4dc3a4a2a9e5f4a690
Jonatandb <jonatandb@gmail.com>
2020-10-15 21:15:56 -0300

    2.11: The Phonebook Step6 (Refactor)
    https://fullstackopen.com/en/part2/getting_data_from_server

--- part2/src/components/App.jsx
+++ part2/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Courses from './courses'
+import Phonebook from './phonebook'
 
 export default function App() {
-    return <Courses />
+    return <Phonebook />
 }
