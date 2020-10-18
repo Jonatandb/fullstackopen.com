4f68f724eece0534c5d338bda14f09c3d76ea431
Jonatandb <jonatandb@gmail.com>
2020-10-18 18:23:32 -0300

    3.12: Command-line database
    https://fullstackopen.com/en/part3/saving_data_to_mongo_db#exercise-3-12

---
+++ part4/phonebook_backend/mongo.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+
+let paramsCount = process.argv.length
+
+if (paramsCount === 2 || paramsCount === 4 || paramsCount > 5) {
+    console.log('Please provide the password as an argument, next (optional) the name and the number: \n node mongo.js <password> "firstname lastname" number \n\n If you just want to list all records run:\n node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+let name = process.argv[3]
+
+let number = process.argv[4]
+
+if (paramsCount === 5) {
+    if (!name || name && name.trim().length === 0) {
+        console.log('Name can\'t be empty')
+        process.exit(1)
+    }
+
+    if (!number || number && number.trim().length === 0) {
+        console.log('Number can\'t be empty')
+        process.exit(1)
+    }
+}
+
+const url =
+    `mongodb+srv://fullstack:${password}@phonebook-cluster-0.pujwt.mongodb.net/phonebook-app?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (paramsCount === 3) {
+
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+
+} else {
+
+    const person = new Person({
+        name,
+        number
+    })
+
+    person.save().then(result => {
+        console.log(`Added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+
+}
