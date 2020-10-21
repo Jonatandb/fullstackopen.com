7578d017ab61477c07e67c464df22ad98bd0177d
Jonatandb <jonatandb@gmail.com>
2020-10-21 00:52:54 -0300

    4.10: Blog list tests, step3
    https://fullstackopen.com/en/part4/testing_the_backend#exercises-4-8-4-12

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -19,6 +19,7 @@ const initialBlogs = [
         likes: 1500
     }
 ]
+
 beforeEach(async () => {
     await Blog.deleteMany({})
     let blogObject = new Blog(initialBlogs[0])
@@ -38,6 +39,26 @@ test('blogs have the unique identifier property id', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body[0].id).toBeDefined()
     expect(response.body[1].id).toBeDefined()
+})
+
+test('new blog is created correctly', async () => {
+    const newBlog = {
+        title: "Full Stack Open 2020",
+        author: "Jonatandb",
+        url: "github.com/Jonatandb",
+        likes: 1005
+    }
+
+    const response = await api.post('/api/blogs').send(newBlog)
+    expect(response.status).toBe(201)
+    expect(response.body.author).toBeDefined()
+    expect(response.body.likes).toBeDefined()
+    expect(response.body.title).toBeDefined()
+    expect(response.body.url).toBeDefined()
+    expect(response.body.id).toBeDefined()
+
+    const blogExistsInDB = await Blog.findById(response.body.id)
+    expect(blogExistsInDB).toBeDefined()
 })
 
 afterAll(() => {
