78d4f00ceec7cd8cfb57c8ed9ef71719c211f19b
Jonatandb <jonatandb@gmail.com>
2020-10-25 02:04:05 -0300

    5.7* Blog list frontend, step7
    https://fullstackopen.com/en/part5/props_children_and_proptypes#exercises-5-5-5-10

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,28 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [open, setOpen] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} <button onClick={() => setOpen(!open)}>{open ? 'Hide' : 'View'}</button><br />
+      {
+        open && <>
+          {blog.url}<br />
+          likes {blog.likes} <button>Like</button><br />
+          {blog.author}<br />
+        </>
+      }
+    </div>
+  )
+}
 
 export default Blog
