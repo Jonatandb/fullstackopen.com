5733d1ead3f2eb361373b329813dabbee9999bd9
Jonatandb <jonatandb@gmail.com>
2020-10-26 23:47:17 -0300

    5.18: bloglist end to end testing, step2
    https://fullstackopen.com/en/part5/end_to_end_testing#exercises-5-17-5-22

--- part5/bloglist-frontend/cypress/integration/blog.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog.spec.js
@@ -1,7 +1,17 @@
 describe('Bloglist app', () => {
 
+  const name = 'Jonatandb testing user'
+  const username = 'Jonatandb_Test'
+  const password = '1234'
+
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name,
+      username,
+      password
+    }
+    cy.request('POST', 'http://localhost:3001/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -10,6 +20,25 @@ describe('Bloglist app', () => {
     cy.contains('Username:')
     cy.contains('Password:')
     cy.contains('Login')
+  })
+
+  describe('Login', () => {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type(username)
+      cy.get('#password').type(password)
+      cy.contains('Login').click()
+      cy.contains(`${username} logged in`)
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type(username)
+      cy.get('#password').type('wrong password')
+      cy.contains('Login').click()
+      cy.contains('Wrong username or password').as('errorMessageDiv')
+      cy.get('@errorMessageDiv').should('have.css', 'border-style', 'solid')
+      cy.get('@errorMessageDiv').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+
   })
 
 })
