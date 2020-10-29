9dfb2fc12f178ed598b4a18354d47b82928e39b3
Jonatandb <jonatandb@gmail.com>
2020-10-29 00:21:23 -0300

    6.8: anecdotes, step6
    https://fullstackopen.com/en/part6/flux_architecture_and_redux#exercises-6-3-6-8

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+    const anecdotes = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    const vote = (id) => {
+        console.log('vote', id)
+        dispatch(voteAnecdote(id))
+    }
+
+    return anecdotes.sort((a, b) => a.votes > b.votes ? -1 : 1).map(anecdote =>
+        <div key={anecdote.id}>
+            <div>
+                {anecdote.content}
+            </div>
+            <div>
+                has {anecdote.votes}
+                &nbsp;<button onClick={() => vote(anecdote.id)}>Vote</button>
+            </div>
+        </div>
+    )
+}
+
+export default AnecdoteList
