73cb4a7232f7f8cc59c3c4b547a73e8aea2e9041
Jonatandb <jonatandb@gmail.com>
2021-04-01 01:07:33 -0300

    8.19 Books by genre, part 1
    https://fullstackopen.com/en/part8/login_and_updating_the_cache#exercises-8-17-8-22

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = props => {
   const allBooks = useQuery(ALL_BOOKS)
+  const [genreToFilter, setGenreToFilter] = useState()
 
   if (!props.show) {
     return null
@@ -13,6 +14,13 @@ const Books = props => {
 
   const books = allBooks.data.allBooks
 
+  const genres = books.reduce((acc, book) => {
+    if (book.genres.length) {
+      book.genres.forEach(genre => !acc.includes(genre) && acc.push(genre))
+    }
+    return acc
+  }, [])
+
   return (
     <div>
       <h2>Books</h2>
@@ -20,19 +28,28 @@ const Books = props => {
       <table>
         <tbody>
           <tr>
-            <th></th>
+            <th>Title</th>
             <th>Author</th>
             <th>Published</th>
           </tr>
-          {books.map(a => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter(b => !genreToFilter || b.genres.includes(genreToFilter))
+            .map(a => (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      <h3>Filter by genre:</h3>
+      {genres.map((g, idx) => (
+        <button key={idx} onClick={() => setGenreToFilter(g)}>
+          {g}
+        </button>
+      ))}
+      {genreToFilter && <button onClick={() => setGenreToFilter('')}>All genres</button>}
     </div>
   )
 }
